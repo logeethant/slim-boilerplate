3ca5b51d5632568e41153e21a515f8f1243f9a4c
Roy Klutman <info@royklutman.nl>
2017-09-16 16:55:59 +0200

    Fixed typo

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -6,7 +6,7 @@ use \App\Models\User;
 
 class HomeController extends \Core\Controller
 {
-    //public function indexAction($request, $response, $args)
+    //public function index($request, $response, $args)
     public function __invoke($request, $response, $args)
     {
         return $this->view->render($response, 'Home/index.html.twig');
